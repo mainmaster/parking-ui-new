fb3379053ce7a2ab2f95858952788aaee86f4c57
pavel.hrudzinski <pavel.hrudzinski@indev.by>
2024-06-17 00:17:18 +0300

    feat: add amount details

--- src/components/PaymentInfoItem/PaymentInfoItem.js
+++ src/components/PaymentInfoItem/PaymentInfoItem.js
@@ -6,10 +6,13 @@ import Input from 'components/Input'
 
 import moment from 'moment'
 import { Formik } from 'formik'
-import { useState } from 'react'
-import { useGlobalSettingsQuery } from '../../api/settings/settings'
+import React, {Fragment, useState} from 'react'
 import { useSubscriptionsQuery } from '../../api/payments.api'
 import { useSearchParams } from 'react-router-dom'
+import {CarNumberCard} from "../CarNumberCard/CarNumberCard";
+import {IconButton, Stack} from "@mui/material";
+import {KeyboardArrowDown, KeyboardArrowUp} from "@mui/icons-material";
+import eventInIcon from '../../assets/svg/log_event_in_icon.svg';
 
 function getTimeFromMins(time) {
   let mins = time % 60
@@ -77,51 +80,131 @@ const PaymentInfoItem = ({
   ImgPath,
   payHandler,
   SessionCreatedAt,
-  sessionId
+  sessionId,
+  AmountDetails,
+  VehiclePlate
 }) => {
 
   const payment = () =>{
     payHandler({sessionId: sessionId, email: ''})
   }
 
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
+  const [isDetailShow, setIsDetailShow] = useState(false);
   const [params] = useSearchParams()
   const {data: subscriptions} = useSubscriptionsQuery(params.get('parkingID'))
 
+  const detailComponent = ({start, end, format, amount, description}) => {
+    return (
+      <div key={start} style={{display: 'flex', gap: 8, fontSize: 16}}>
+        <div style={{color:'rgb(126, 122, 131)', width: '30%'}}>
+          {moment(start).format(format)}-{moment(end).format(format)}
+        </div>
+        <div style={{color: `${amount !== 0 ? '' : 'rgb(77, 143, 89)'}`}}>
+          {amount}₽
+        </div>
+        <div style={{color:'rgb(126, 122, 131)'}}>
+          ({description})
+        </div>
+      </div>
+    )
+  }
 
   return (
     <Card>
-      <SubmitModal 
-        payHandler={payHandler} 
-        show={show} 
+      <SubmitModal
+        payHandler={payHandler}
+        show={show}
         sessionId={sessionId}
-        handleClose={()=>setShow(false)}
+        handleClose={() => setShow(false)}
       />
-      
+
       <Card.Img
         variant="top"
         src={process.env.REACT_APP_API_URL + '/' + ImgPath}
         style={{ maxHeight: 366 }}
       />
       <Card.Body>
-        <Card.Title>
-          {Name}, {moment(SessionCreatedAt).format('DD/MM/YY HH:mm')}
-        </Card.Title>
-        <Card.Text>
-          <span>Сумма:</span> {Amount} руб.
-          <br />
-          <span>Время проведенное на парковке:</span>{' '}
-          {getTimeFromMins(TotalTimeMin)}
-        </Card.Text>
-        <Button variant="primary" onClick={()=>{
+        <Stack direction={'column'} gap={'16px'}>
+          <Stack direction={'row'} justifyContent={'space-between'}>
+            <CarNumberCard carNumber={VehiclePlate} isTable />
+            <div style={{width: 48, height: 40, display: "flex", alignItems: 'center', justifyContent: 'center', border: '1px solid black', borderRadius: 8, cursor: 'pointer'}} onClick={() => setIsDetailShow(!isDetailShow)}>
+              {isDetailShow ? <KeyboardArrowUp/> : <KeyboardArrowDown/>}
+            </div>
+          </Stack>
+          {isDetailShow && AmountDetails?.length && AmountDetails.length > 0 ?
+            <Stack direction={'column'} gap={'4px'}>
+              <div style={{display: 'flex', gap: 8, fontSize: 16}}>
+                <div style={{color: 'rgb(126, 122, 131)', width: '30%'}}>
+                  {moment(SessionCreatedAt).format('hh:mm, DD.MM')}
+                </div>
+                <img style={{width: '18px'}} src={eventInIcon}/>
+                <div>
+                  Въезд
+                </div>
+              </div>
+              {AmountDetails.map((detail) => {
+                if (detail.type === 'free_time_min') {
+                  return (
+                    detailComponent({
+                      start: detail.start_hour,
+                      end: detail.end_hour,
+                      format: 'hh:mm',
+                      amount: detail.amount,
+                      description: 'первые 30м'
+                    })
+                  )
+                }
+
+                if (detail.type === 'pay_by_first_hours') {
+                  return (
+                    detailComponent({
+                      start: detail.start_hour,
+                      end: detail.end_hour,
+                      format: 'hh:mm',
+                      amount: detail.amount,
+                      description: 'первые 3ч'
+                    })
+                  )
+                }
+
+                if (detail.type === 'pay_by_hour') {
+                  return (
+                    detailComponent({
+                      start: detail.start_hour,
+                      end: detail.end_hour,
+                      format: 'hh:mm',
+                      amount: detail.amount,
+                      description: '100₽/ч'
+                    })
+                  )
+                }
+
+                if (detail.type === 'pay_by_day') {
+                  return (
+                    detailComponent({
+                      start: detail.start_hour,
+                      end: detail.end_hour,
+                      format: 'DD.MM',
+                      amount: detail.amount,
+                      description: '1000₽/д'
+                    })
+                  )
+                }
+              })}
+              <span>Время проведенное на парковке: {getTimeFromMins(TotalTimeMin)}</span>
+            </Stack>
+            : <></>}
+          <Button variant="primary" style={{width: '100%'}} onClick={()=>{
             if(subscriptions.emailForPayment){
               setShow(true)
             }else{
               payment()
             }
-        }}>
-          Оплатить
-        </Button>
+          }}>
+            Оплатить {Amount} ₽
+          </Button>
+        </Stack>
       </Card.Body>
     </Card>
   )
