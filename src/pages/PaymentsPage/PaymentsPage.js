8aa0c340db6188f61415ee118a39d7d39843641f
pavel.hrudzinski <pavel.hrudzinski@indev.by>
2024-06-28 23:20:27 +0300

    feat: add yandex metrica

--- src/pages/PaymentsPage/PaymentsPage.js
+++ src/pages/PaymentsPage/PaymentsPage.js
@@ -48,6 +48,31 @@ const PaymentsPage = () => {
   const containerRef = useRef(null);
   const [itemsInRow, setItemsInRow] = useState(0);
 
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.innerHTML = `
+      (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+      })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+      ym(97631905, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+      });
+    `;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   const totalTextStyle = useMemo(() => {
     return {
       fontSize: '1.5rem',
