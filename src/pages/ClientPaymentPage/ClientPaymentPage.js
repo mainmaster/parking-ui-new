e857f1478cde60a4b56cd021efa63d600287f524
pavel.hrudzinski <pavel.hrudzinski@indev.by>
2024-06-28 23:22:21 +0300

    feat: add yandex metrica

--- src/pages/ClientPaymentPage/ClientPaymentPage.js
+++ src/pages/ClientPaymentPage/ClientPaymentPage.js
@@ -151,6 +151,30 @@ const ClientPaymentPage = () => {
 
   const { data: footerInfo } = useGetInfoFooterQuery(parkingID)
 
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.innerHTML = `
+      (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+      })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+      ym(97631905, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+      });
+    `;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
 
   return (
       <div className={css.clientPaymentPageWrapper}>
