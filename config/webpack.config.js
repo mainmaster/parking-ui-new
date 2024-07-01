159f86ab1d79f17ba95ac0d4c12daa71799d8d8c
pavel.hrudzinski <pavel.hrudzinski@indev.by>
2024-07-01 09:30:31 +0300

    feat: remove devtool

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -192,9 +192,7 @@ module.exports = function (webpackEnv) {
     // Stop compilation early in production
     bail: isEnvProduction,
     devtool: isEnvProduction
-      ? shouldUseSourceMap
-        ? 'source-map'
-        : false
+      ? false
       : isEnvDevelopment && 'cheap-module-source-map',
     // These are the "entry points" to our application.
     // This means they will be the "root" imports that are included in JS bundle.
